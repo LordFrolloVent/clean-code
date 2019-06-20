e9ede959bd06e33e6de745abb8ed53d4028d2b06
Étienne Arcand <arcandet@gmail.com>
2019-06-19 20:32:09 -0400

    oupsi 1.1

--- app.py
+++ app.py
@@ -2,19 +2,24 @@ from flask import Flask, jsonify
 
 import json
 
-
-app = Flask(__name__)
-
 from jsonify import convert
 convert.jsonify('quotes.csv')
 
-
-@app.route('/quotes.json', methods=['GET'])
-def get_tasks():
-    return ({'tasks': tasks})
-
-
-
 with open('quotes.json') as json_file:  
     data = json.load(json_file)
-        
+
+    print(data)
+
+
+    
+
+
+# @app.route('/quotes.json', methods=['GET'])
+# def get_tasks():
+#     return ({'tasks': tasks})
+
+
+
+# with open('quotes.json') as json_file:  
+#     data = json.load(json_file)
+       
